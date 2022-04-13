ed711f08a1f408bb25cc6cf091b7924f942acb5a
Eremey Malinovsky <Elerphore@gmail.com>
2022-04-13 23:59:57 +0500

    Boolean

--- lecture-one/boolean.rb
+++ lecture-one/boolean.rb
@@ -2,3 +2,13 @@
 some_boolean = true
 puts "The variable: #{some_boolean} | class: #{some_boolean.class}"
 
+if some_boolean
+  puts "The some_boolean variable is equal to #{some_boolean}"
+else
+  puts "The some_boolean variable is equal to #{some_boolean}"
+end
+
+puts (3 > 2).class
+puts (3 < 2).class
+puts ('some_string'.eql? 'some_string')
+puts ('some_string'.eql? 'some_other_string')
